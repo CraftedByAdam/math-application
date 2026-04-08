87c865c5bcc235bcb38e630a3ba72ebc10d0b600
Your Name <youremail@example.com>
2026-04-08 10:18:18 -0400

    Completed Question 1 and found the highest salary and printed the result.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+
+        //Question 1
+        double bobSalary = 800.00;
+        double garySalary = 200.00;
+        double highestSalary;
+
+        Math.max(bobSalary, garySalary);
+        highestSalary = Math.max(bobSalary, garySalary);
+
+        System.out.println("Highest salary is: " + highestSalary);
+
+    }
 }
