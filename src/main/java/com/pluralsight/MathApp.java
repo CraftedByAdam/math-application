f5b14c9e1afde1e9d7d4c46aca6213e3aacc5930
Your Name <youremail@example.com>
2026-04-08 10:29:46 -0400

    Completed Question 2 and found the lowest priced vehicle and printed the result.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -4,6 +4,8 @@ public class MathApp {
     public static void main(String[] args) {
 
         //Question 1
+        System.out.println("Question 1:");
+
         double bobSalary = 800.00;
         double garySalary = 200.00;
         double highestSalary;
@@ -13,5 +15,19 @@ public class MathApp {
 
         System.out.println("Highest salary is: " + highestSalary);
 
+        System.out.println("==========================================================");
+
+        //Question 2
+        System.out.println("Question 2:");
+
+        double carPrice = 1000.00;
+        double truckPrice = 5000.00;
+        double lowestVehiclePrice;
+
+        Math.min(carPrice, truckPrice);
+        lowestVehiclePrice = Math.min(carPrice, truckPrice);
+        System.out.println("Lowest vehicle price is: " + lowestVehiclePrice);
+
+        System.out.println("==========================================================");
     }
 }
